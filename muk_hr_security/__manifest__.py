b882099d5baca6553a998cd2f5f6cfe695c39a59
Mathias Markl <mathias.markl@mukit.at>
2018-07-05 13:28:03 +0200

    ..

--- muk_hr_security/__manifest__.py
+++ muk_hr_security/__manifest__.py
@@ -21,7 +21,7 @@
     "name": "MuK HR Security",
     "summary": """Security Features""",
     "version": "11.0.1.0.0",
-    "category": "Extra Tools",
+    "category": "Hidden",
     "license": "AGPL-3",
     "website": "http://www.mukit.at",
     "live_test_url": "https://demo.mukit.at/web/login",
