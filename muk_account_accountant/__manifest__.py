ddbee75375c478f3d3b5edae9b57b0f2490d1518
MuK IT GmbH <mathias.markl@mukit.at>
2018-08-15 18:00:06 +0000

    publish muk_account_accountant - 12.0

--- muk_account_accountant/__manifest__.py
+++ muk_account_accountant/__manifest__.py
@@ -1,5 +1,5 @@
 ###################################################################################
-# 
+#
 #    Copyright (C) 2018 MuK IT GmbH
 #
 #    This program is free software: you can redistribute it and/or modify
@@ -13,7 +13,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ###################################################################################
 
